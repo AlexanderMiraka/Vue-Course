5282f0ed4778a1f4fed65ed5006fa4bec61376bb
Alexander Miraka <amiraka@deloitte.gr>
2024-01-16 15:53:24 +0200

    Watchers and computed objects

--- app.js
+++ app.js
@@ -3,9 +3,29 @@ const app = Vue.createApp({
     return {
       counter: 0,
       name:'',
+      lastName: '',
     };
   },
-  methods: {
+  computed: {//to print data
+    fullName() {
+      if(this.name==='') {
+        return '';
+      }
+      return this.name + '' + 'test';
+    },
+  },
+  watch: {
+    counter(value) {//you don't have to specify the param because vue does it automatically
+      if(value > 50) {
+        const that = this;
+        setTimeout(function () {
+          that.counter = 0;
+        },2000);
+      }
+      this.fullName = value + this.lastName;
+    },//whenever counter changes name is executed
+  },
+  methods: {//for event listeners it refreshes everytime anything changes on page.
     setName: function(event) {//default param for functions
         this.name = event.target.value;
     },
@@ -17,6 +37,9 @@ const app = Vue.createApp({
     },
     remove: function (num) {
         this.counter -= num;
+    },
+    resetInput: function () {
+      this.name = '';
     },
   },
 });
